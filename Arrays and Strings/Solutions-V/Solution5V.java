cc777ba56929c4f66ba6bab0d000b7cf330f452d
viveka aggarwal <aggarwalviveka@gmail.com>
2021-01-25 22:53:20 -0800

    description update for sol 5

--- Arrays and Strings/Solutions-V/Solution5V.java
+++ Arrays and Strings/Solutions-V/Solution5V.java
@@ -3,7 +3,7 @@
  * Write a method to replace all spaces in a string with'%20'. You may assume that the string has sufficient space at the end of the string to hold the additional characters, and that you are given the "true" length of the string.
  * (Note: for Java, please use a character array so that you can perform this operation in place.)
  * EXAMPLE
- * Input: "Mr John Smith" Output: "Mr%20Dohn%20Smith"
+ * Input: "Mr John Smith" Output: "Mr%20John%20Smith"
  */
 
 public class Solution5V {
